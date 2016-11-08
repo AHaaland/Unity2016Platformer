86f2918de292fcde7fb69910a84fce9f24ba3621
n02762252 <haalanda1@hawkmail.newpaltz.edu>
2016-11-08 18:09:43 -0500

    Added shrink feature on down arrow press

--- Assets/Scripts/Player.cs
+++ Assets/Scripts/Player.cs
@@ -31,6 +31,11 @@ public class Player : MonoBehaviour {
         {
             rigidbody2D.velocity += Vector2.up * jumpForce;
         }
+        if(Input.GetAxis("Vertical") < 0)
+        {
+            transform.localScale *= .5f;
+        }
+
         if(rigidbody2D.velocity.x > 0)
         {
             transform.rotation = new Quaternion(0, 0, 0, 0);
