6894ec5d0a81af67e4bcb6c24eab895aae9ef2d5
n02762252 <haalanda1@hawkmail.newpaltz.edu>
2016-11-17 18:12:40 -0500

    Updated game to test 3 types of enemies

--- Assets/Scripts/HeatSeekingMovement.cs
+++ Assets/Scripts/HeatSeekingMovement.cs
@@ -3,9 +3,17 @@ using System.Collections;
 
 public class HeatSeekingMovement : MonoBehaviour
 {
+    public Transform target;
+    void Start()
+    {
+        if(target==null)
+        {
+            target = FindObjectOfType<Player>().transform;
+        }
+    }
     void Update()
     {
-        transform.position = Vector3.MoveTowards(transform.position, FindObjectOfType<Player>().transform.position, Time.deltaTime);
+        transform.position = Vector3.Slerp(transform.position, target.position, Time.deltaTime);
 
     }
 }
